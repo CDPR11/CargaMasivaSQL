813bb2ba73424d65eefac025b1bf604639027cc8
Daniel Rodriguez <20193tn018@utez.edu.mx>
2023-12-11 17:26:13 -0600

    App finished

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
 const excelInput = document.getElementById("excel-input");
+const databaseInput = document.getElementById("database-input");
 const contentXls = document.getElementById("content-xls");
 const btnExportDocument = document.getElementById("btnExportDocument");
 const btnClear = document.getElementById("btnClear");
 const tableInput = document.getElementById("table-input")
+const divChecks = document.getElementById("checksD")
+const btnRealizarScript = document.getElementById("btnRealizarScript")
+
+// const btnCheck = document.getElementById("btn-check")
 contentXls.value = ""
 class Excel{
     constructor(content){
@@ -24,57 +29,163 @@ excelInput.addEventListener('change',async function(){
     
     const excel = new Excel(content)
     let texto = ""
+    let arrayChecks = []
     let tableName = tableInput.value
     for(let i = 0; i < excel.header().length; i++){
         if( i === (excel.header().length - 1))
             texto = texto + excel.header()[i] + ""
         else    
             texto = texto + excel.header()[i] + ","
+        arrayChecks.push(excel.header()[i])
     }
     
+    // let contentRow = ''
+    // let estructura =''
+    // for(let i = 0; i < excel.rows().length; i++){
+    //     contentRow = ""
+    //     for( let j = 0; j < excel.rows()[i].length; j++){
+    //         if(typeof(excel.rows()[i][j]) === 'object'){
+    //             excel.rows()[i][j] = formatearFecha(excel.rows()[i][j])
+    //         }
+
+    //         if(j === excel.rows()[j].length - 1 )
+    //             contentRow = `${contentRow} ${excel.rows()[i][j].toString().toLowerCase().includes('null') || excel.rows()[i][j].toString().toLowerCase().includes('getdate()') ?excel.rows()[i][j]:`'${excel.rows()[i][j]}'`}` 
+    //         else
+    //             contentRow = `${contentRow} ${excel.rows()[i][j].toString().toLowerCase().includes('null') || excel.rows()[i][j].toString().toLowerCase().includes('getdate()') ?excel.rows()[i][j]:`'${excel.rows()[i][j]}'`},`
+            
+    //     }
+    //     //console.log(contentRow)
+    //     estructura = `${estructura}\n       IF NOT EXISTS ( SELECT 1 FROM ${tableInput.value} WHERE Campo = Valor)
+    //     BEGIN
+    //         INSERT INTO ${tableName} (${texto}) VALUES (${contentRow}); 
+    //     END
+    //     ELSE
+    //     BEGIN
+    //         PRINT('Este registro ya existe');
+    //     END`
+        
+    // }
+
+    console.log(arrayChecks)
+    let checksButtons =''
+    for(let i = 0; i < arrayChecks.length; i++){
+        checksButtons = `${checksButtons}\n<div class="col-4 col-md-4 col-lg-3">
+                            <input type="checkbox" class="btn-check" id="btn-check${i}" autocomplete="off">
+                            <label class="btn btn-primary" for="btn-check">${arrayChecks[i]}</label>
+                        </div>`    
+    }
+    divChecks.innerHTML = checksButtons;
+
+
+    // contentXls.value = estructura
+})
+
+btnRealizarScript.addEventListener('click', async function(){
+    const content = await readXlsxFile(excelInput.files[0])
+    
+    const excel = new Excel(content)
+    let texto = ""
+    let arrayChecks = []
+    let tableName = tableInput.value
+    for(let i = 0; i < excel.header().length; i++){
+        if( i === (excel.header().length - 1))
+            texto = texto + `[${excel.header()[i]}]` + ""
+        else    
+            texto = texto + `[${excel.header()[i]}]` + ","
+        arrayChecks.push(excel.header()[i])
+    }
+    
+    let arrayCheckeds = []
+    for(let i = 0; i < arrayChecks.length; i++){
+        
+        if(document.getElementById('btn-check'+i).checked){
+            let json = {
+                "posicion":i,
+                "name":arrayChecks[i]
+            }
+            arrayCheckeds.push(json)
+        }
+    }
+
+
+    console.log(arrayCheckeds)
+    let whereString = ''
     let contentRow = ''
+    let estructura =`USE [${databaseInput.value}]
+    GO
+    BEGIN TRANSACTION;
+        BEGIN TRY\n`
     for(let i = 0; i < excel.rows().length; i++){
+        contentRow = ""
+        whereString = ""
         for( let j = 0; j < excel.rows()[i].length; j++){
-            //console.log(excel.rows()[j].length)
+            if(arrayCheckeds.length > 0){
+                for(let k = 0; k < arrayCheckeds.length; k++){
+                    if(arrayCheckeds[k].posicion === j){
+                        whereString = `${whereString} ${arrayCheckeds[k].name} = '${excel.rows()[i][j]}' ${k != (arrayCheckeds.length - 1)?'AND':''}`;
+                    }
+                }
+            }
+            if(typeof(excel.rows()[i][j]) === 'object'){
+                excel.rows()[i][j] = formatearFecha(excel.rows()[i][j])
+            }
 
             if(j === excel.rows()[j].length - 1 )
-                contentRow = content + `'${excel.rows()[i][j]}'` 
+                contentRow = `${contentRow} ${excel.rows()[i][j].toString().toLowerCase().includes('null') || excel.rows()[i][j].toString().toLowerCase().includes('getdate()') ?excel.rows()[i][j]:`'${excel.rows()[i][j]}'`}` 
             else
-                contentRow = content + `'${excel.rows()[i][j]}'` + ","
+                contentRow = `${contentRow} ${excel.rows()[i][j].toString().toLowerCase().includes('null') || excel.rows()[i][j].toString().toLowerCase().includes('getdate()') ?excel.rows()[i][j]:`'${excel.rows()[i][j]}'`},`
+            
         }
-        console.log(contentRow)
-        contentRow = ""
-    }
-
-    //console.log(contentRow)
-    // let arrayValidaciones = ['ClaveCodigoTransporteAereo','VersionComplemento']
-    // let whereString = '';
-    // if(arrayValidaciones.length > 0){
-    //     if(arrayValidaciones.length>1){
-    //         for(let i = 0; i < arrayValidaciones.length; i++){
-    //             for(let j = 0; j<=)
-    //             whereString = whereString + arrayValidaciones[i] + 'AND'
-    //         }
-    //     }else{
-    //         whereString = arrayValidaciones[0]
-    //     }
-    // }
-    
-
-    let estructura = `IF NOT EXISTS ( SELECT 1 FROM Tabla WHERE Campo = Valor)
+        console.log(whereString)
+        //console.log(contentRow)
+        estructura = `${estructura}\n       IF NOT EXISTS ( SELECT 1 FROM ${tableInput.value} WHERE ${whereString})
         BEGIN
-            INSERT INTO ${tableName} (${texto}) VALUES (value1, value2, value3, ...); 
+            INSERT INTO [dbo].[${tableName}] (${texto}) VALUES (${contentRow}); 
+            PRINT('Se inserto con exito el registro ${whereString.replaceAll("'","")}')
         END
         ELSE
         BEGIN
-            PRINT('Este registro ya existe');
+            PRINT('El registro con ${whereString.replaceAll("'","")} ya existe');
         END`
-
-    console.log(texto)
-    contentXls.value = estructura
-    // console.log(excel.header())
-    // console.log(excel.rows())
+        
+    }
+    estructura = estructura + `\nEND TRY 
+    BEGIN CATCH
+        SELECT 'Insert Incorrecto'  AS [Mensaje ejecución]   ,CAST (ERROR_NUMBER() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_NUMBER()  ]
+        ,CAST (ERROR_SEVERITY() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_SEVERITY() ]   ,CAST (ERROR_STATE() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_STATE()  ]
+        ,CAST (ERROR_PROCEDURE() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_PROCEDURE()]   ,CAST (ERROR_LINE() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_LINE()   ]
+        ,CAST (ERROR_MESSAGE() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_MESSAGE() ]  
+        IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+    END CATCH
+        IF @@TRANCOUNT > 0 -- Verifica correcta ejecucion de nivel de transaccion
+    BEGIN
+        COMMIT TRANSACTION;  
+        SELECT 'Insert Ejecutado';
+    END`;
+    contentXls.value = sqlFormatter.format(estructura)
 })
+
+function formatearFecha(fechaStr) {
+    var fecha = new Date(fechaStr);
+    
+    fecha.setUTCHours(0, 0, 0, 0);
+    var year = fecha.getUTCFullYear()
+    var month = ('0' + (fecha.getUTCMonth() + 1)).slice(-2);
+    var day = ('0' + fecha.getUTCDate()).slice(-2);
+
+    var formattedDate = year + '-' + month + '-' + day;
+
+    return formattedDate;
+}
+
+const copiarContenido = async () => {
+    try {
+      await navigator.clipboard.writeText(contentXls.value);
+      alert('Contenido copiado al portapapeles');
+    } catch (err) {
+      console.error('Error al copiar: ', err);
+    }
+}
 
 btnExportDocument.addEventListener('click', function(){
    if(contentXls.value){
@@ -86,17 +197,22 @@ btnExportDocument.addEventListener('click', function(){
 })
 
 btnClear.addEventListener('click', function(){
-    console.log("clickea limpiar")
     contentXls.value = ""
 })
 
 function exportarTexto(content){
     var fileContent = content;
-    var fileName = 'sampleFile.txt';
+    var fileName = 'sampleFile.sql';
 
     const blob = new Blob([fileContent], { type: 'text/plain' });
     const a = document.createElement('a');
     a.setAttribute('download', fileName);
     a.setAttribute('href', window.URL.createObjectURL(blob));
     a.click();
+
+
 }
+
+// btnCheck.addEventListener('click', function(){
+//     console.log("entra al check"+ btnCheck.checked)
+// })
