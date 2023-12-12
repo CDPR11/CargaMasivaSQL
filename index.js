b8e3df9fe2b40392300d0eda28b4c1c780f892b0
mmartinezfx <142332567+mmartinezfx@users.noreply.github.com>
2023-12-12 14:11:25 -0600

    [ADD] Delete

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const btnClear = document.getElementById("btnClear");
 const tableInput = document.getElementById("table-input")
 const divChecks = document.getElementById("checksD")
 const btnRealizarScript = document.getElementById("btnRealizarScript")
+const selectTipoSQL = document.getElementById("tipoSQL")
 let nombreDocumento = ''
-
+let tipoSentencia = ''
+let sentencia = ''
+let tableName = ''
 contentXls.value = ""
+
 class Excel{
     constructor(content){
         this.content = content;
@@ -23,6 +27,20 @@ class Excel{
     }
 }
 
+let finalCatch = `\nEND TRY 
+    BEGIN CATCH
+        SELECT 'Insert Incorrecto'  AS [Mensaje ejecución]   ,CAST (ERROR_NUMBER() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_NUMBER()  ]
+        ,CAST (ERROR_SEVERITY() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_SEVERITY() ]   ,CAST (ERROR_STATE() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_STATE()  ]
+        ,CAST (ERROR_PROCEDURE() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_PROCEDURE()]   ,CAST (ERROR_LINE() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_LINE()   ]
+        ,CAST (ERROR_MESSAGE() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_MESSAGE() ]  
+        IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+    END CATCH
+        IF @@TRANCOUNT > 0 -- Verifica correcta ejecucion de nivel de transaccion
+    BEGIN
+        COMMIT TRANSACTION;  
+        SELECT 'Insert Ejecutado';
+    END`;
+  
 excelInput.addEventListener('change',async function(){
     const content = await readXlsxFile(excelInput.files[0])
     
@@ -50,13 +68,51 @@ excelInput.addEventListener('change',async function(){
     divChecks.innerHTML = checksButtons;
 })
 
+selectTipoSQL.addEventListener('change', function (){
+	
+	switch(document.getElementById('tipoSQL').value)
+    {
+		case '1' : 
+			tipoSentencia = 'INSERT'
+			break;
+		case '2' :
+			tipoSentencia = 'UPDATE'
+			break;
+		case '3':
+			tipoSentencia = 'DELETE'
+			break;
+		default:
+			tipoSentencia = 'INSERT'
+	}
+
+})
+
 btnRealizarScript.addEventListener('click', async function(){
     const content = await readXlsxFile(excelInput.files[0])
-    
     const excel = new Excel(content)
-    let texto = ""
+    
+	tableName = tableInput.value
+	
+	switch(tipoSentencia)
+	{
+		case 'INSERT' : 
+			crearInsert(excel)
+			break;
+		case 'UPDATE' :
+			
+			break;
+		case 'DELETE':
+			crearDelete(excel)
+			break;
+		default:
+			crearInsert(excel)
+	}
+})
+
+function crearInsert (param){
+	let excel = param
+	let texto = ""
     let arrayChecks = []
-    let tableName = tableInput.value
     for(let i = 0; i < excel.header().length; i++){
         if( i === (excel.header().length - 1))
             texto = texto + `[${excel.header()[i]}]` + ""
@@ -94,15 +150,23 @@ btnRealizarScript.addEventListener('click', async function(){
                     }
                 }
             }
+			
             if(typeof(excel.rows()[i][j]) === 'object'){
                 excel.rows()[i][j] = excel.rows()[i][j] != null ? formatearFecha(excel.rows()[i][j]) : ""
             }
 
-            if(j === excel.rows()[j].length - 1 )
+            /*if(j === excel.rows()[j].length - 1 )
                 contentRow = `${contentRow} ${excel.rows()[i][j].toString().toLowerCase().includes('null') || excel.rows()[i][j].toString().toLowerCase().includes('getdate()') ?excel.rows()[i][j]:`'${excel.rows()[i][j]}'`}` 
             else
                 contentRow = `${contentRow} ${excel.rows()[i][j].toString().toLowerCase().includes('null') || excel.rows()[i][j].toString().toLowerCase().includes('getdate()') ?excel.rows()[i][j]:`'${excel.rows()[i][j]}'`},`
-            
+            */
+			const cellValue = excel.rows()[i][j];
+			const formattedCellValue = cellValue.toString().toLowerCase().includes('null') || cellValue.toString().toLowerCase().includes('getdate()')
+				? cellValue
+				: `'${cellValue}'`;
+
+			contentRow += (j === excel.rows()[j].length - 1) ? formattedCellValue : `${formattedCellValue},`;
+
         }
 
         estructura = `${estructura}\n       IF NOT EXISTS ( SELECT 1 FROM ${tableInput.value} WHERE ${whereString})
@@ -117,22 +181,70 @@ btnRealizarScript.addEventListener('click', async function(){
         
     }
 
-    estructura = estructura + `\nEND TRY 
-    BEGIN CATCH
-        SELECT 'Insert Incorrecto'  AS [Mensaje ejecución]   ,CAST (ERROR_NUMBER() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_NUMBER()  ]
-        ,CAST (ERROR_SEVERITY() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_SEVERITY() ]   ,CAST (ERROR_STATE() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_STATE()  ]
-        ,CAST (ERROR_PROCEDURE() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_PROCEDURE()]   ,CAST (ERROR_LINE() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_LINE()   ]
-        ,CAST (ERROR_MESSAGE() AS NVARCHAR(1000)) AS [Dato_Error_ERROR_MESSAGE() ]  
-        IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
-    END CATCH
-        IF @@TRANCOUNT > 0 -- Verifica correcta ejecucion de nivel de transaccion
-    BEGIN
-        COMMIT TRANSACTION;  
-        SELECT 'Insert Ejecutado';
-    END`;
+    estructura = estructura + finalCatch
     contentXls.value = sqlFormatter.format(estructura)
-})
+}
 
+function crearDelete (param){
+	let excel = param
+    let arrayChecks = [] 
+	let contentRow = ''
+	
+    for(let i = 0; i < excel.header().length; i++){
+		contentRow = `${contentRow} deleted.${excel.header()[i]} AS ${excel.header()[i]}_old${i != (excel.header().length - 1) ? ',':''}`
+        arrayChecks.push(excel.header()[i])
+    }
+    
+    let arrayCheckeds = []
+    for(let i = 0; i < arrayChecks.length; i++){
+        
+        if(document.getElementById('btn-check'+i).checked){
+            let json = {
+                "posicion":i,
+                "name":arrayChecks[i]
+            }
+            arrayCheckeds.push(json)
+        }
+    }
+	
+    let whereString = ''
+    let estructura =`USE [${databaseInput.value}]
+    GO
+    BEGIN TRANSACTION;
+        BEGIN TRY\n`;
+	
+    for(let i = 0; i < excel.rows().length; i++){
+        whereString = ""
+        for( let j = 0; j < excel.rows()[i].length; j++){
+			if(arrayCheckeds.length > 0){
+                for(let k = 0; k < arrayCheckeds.length; k++){
+                    if(arrayCheckeds[k].posicion === j){
+                        whereString = `${whereString} ${arrayCheckeds[k].name} = '${excel.rows()[i][j]}' ${k != (arrayCheckeds.length - 1)?'AND':''}`;
+                    }
+                }
+            }
+        }
+
+		sentencia = `DELETE FROM [dbo].[${tableName}]
+							 OUTPUT ${contentRow}
+							 WHERE ${whereString}
+							 PRINT('Se eliminó con exito el registro ${whereString.replaceAll("'","")}')`
+		
+        estructura = `${estructura}\n       IF EXISTS ( SELECT 1 FROM ${tableInput.value} WHERE ${whereString})
+        BEGIN
+            ${sentencia}
+        END
+        ELSE
+        BEGIN
+            PRINT('El registro con ${whereString.replaceAll("'","")} no existe');
+        END`
+        
+    }
+
+    estructura = estructura + finalCatch
+    contentXls.value = sqlFormatter.format(estructura)
+}
+	
 function formatearFecha(fechaStr) {
     var fecha = new Date(fechaStr);
     
@@ -177,6 +289,4 @@ function exportarTexto(content){
     a.setAttribute('download', fileName);
     a.setAttribute('href', window.URL.createObjectURL(blob));
     a.click();
-
-
 }
